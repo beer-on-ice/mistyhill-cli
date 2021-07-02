c7baf7091afbf97fff50213195356c7f5bf14703
lijin <li997477295@outlook.com>
2021-07-02 14:45:18 +0800

    fix

--- packages/core/lib/index.js
+++ packages/core/lib/index.js
@@ -6,6 +6,7 @@ const packageJson = require('../package')
 const { log } = require('@mistyhill-cli/utils')
 
 function core() {
+  console.log()
   console.log(` _   _             __  __ _                    ____ _ _ 
 | | | |_   _  __ _|  \\/  (_) __ _  ___        / ___| (_)
 | |_| | | | |/ _\` | |\\/| | |/ _\` |/ _ \\  ___ | |   | | |
@@ -13,4 +14,13 @@ function core() {
 |_| |_|\\__,_|\\__,_|_|  |_|_|\\__,_|\\___/       \\____|_|_|  Version ${packageJson.version}
 `)
   log.info('欢迎使用')
+  console.log()
+
+  const semver = require('semver')
+  const MINIMUM_NODEJS_VERSION = '14.0.0'
+
+  if (semver.lte(process.version, MINIMUM_NODEJS_VERSION)) {
+    log.error(`mistyhill-cli 最低要求Node.js版本 v${MINIMUM_NODEJS_VERSION}`)
+    process.exit()
+  }
 }
--- packages/utils/lib/log.js
+++ packages/utils/lib/log.js
@@ -1,9 +1,43 @@
 const log = require('npmlog')
 
-log.level = 'info'
+const minimist = require('minimist')
+const program = require('commander')
 
-log.heading = 'mistyhill-cli' // 自定义头部
-log.addLevel('success', 2000, { fg: 'green', bold: true }) // 自定义success日志
-log.addLevel('notice', 2000, { fg: 'blue', bg: 'black' }) // 自定义notice日志
+log.verbose('校验入参：')
+let args = minimist(process.argv.slice(2))
+log.verbose('args: ', args)
+
+if (args['_'].length === 0 && !args.debug) {
+  log.warn('请输入参数')
+} else {
+  // 可以在这配置debug相关设置，比如修改log的等级为verbose来打印调试日志
+  if (args.debug) process.env.DEBUGGING = 'verbose'
+  else process.env.DEBUGGING = 'info'
+  log.level = process.env.DEBUGGING
+}
+
+// 设置版本号、自定义用法说明
+program.version(packageJson.version).usage('<command> [options] 其他说明')
+
+// 添加命令可以在这里添加
+program
+  .command('test')
+  .description('描述')
+  .option('-a, --all', '清空全部')
+  .action((cmd, options) => {
+    // cmd.all 自动由上面配置 .option("-a, --all", "清空全部") 创建
+    console.log('cmd: ', cmd.all)
+    log.success('测试', '一只花喵')
+  })
+
+// 其他子命令
+program.command('*').action(function(cmd, options) {
+  console.log('cmd: ', cmd)
+  console.log('options: ', options.args)
+  console.log('没有匹配到命令：miao', args['_'])
+})
+
+// 注册命令
+program.option('--debug', '打开调试模式').parse(process.argv)
 
 module.exports = log
