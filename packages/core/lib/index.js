79cc181f39640f942ce63f4e2f1db432652ab69f
lijin <li997477295@outlook.com>
2021-07-02 14:29:11 +0800

    ficx

--- packages/core/lib/index.js
+++ packages/core/lib/index.js
@@ -1,1 +1,16 @@
-console.log('hello world!')
+'use strict'
+
+module.exports = core
+
+const packageJson = require('../package')
+const { log } = require('@mistyhill-cli/utils')
+
+function core() {
+  console.log(` _   _             __  __ _                    ____ _ _ 
+| | | |_   _  __ _|  \\/  (_) __ _  ___        / ___| (_)
+| |_| | | | |/ _\` | |\\/| | |/ _\` |/ _ \\  ___ | |   | | |
+|  _  | |_| | (_| | |  | | | (_| | (_) ||___|| |___| | |
+|_| |_|\\__,_|\\__,_|_|  |_|_|\\__,_|\\___/       \\____|_|_|  Version ${packageJson.version}
+`)
+  log.info('欢迎使用')
+}
